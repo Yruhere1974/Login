dfafc4ced55f6c0467edb071dc70024695c66e31
simon <simon@croprecords.com>
2018-09-07 15:50:05 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+	class Program
+	{
+		static string myBatchFileName { get { return "brainstart.bat"; } } //This is the name of the file we are going to run.  We have made it static which means ir
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome!!");
+			Console.Write("Enter User Name:");
+			string UserName = Console.ReadLine();
+			Console.Write("Enter Password:");
+			string Password = Console.ReadLine();
+			string Permission = Security.TestCredentials(UserName, Password);
+			Console.WriteLine("Permission Level:" + Permission);
+			if (Permission == "ADMIN")
+			{
+				AdminPermissions();
+			}
+			if (Permission == "NORMAL")
+			{
+				RunBatchFile();
+			}
+
+		}
+		static void AdminPermissions()
+		{
+			Console.WriteLine("1   Add Login");
+			Console.WriteLine("2   Run File");
+			Console.Write("Choice:");
+			ConsoleKeyInfo key = Console.ReadKey();
+			Console.WriteLine(key.KeyChar);
+			if (key.KeyChar =='1')
+			{
+				AddUser();
+			}
+			else
+			{
+				RunBatchFile();
+			}
+			
+			
+			
+
+
+		}
+		static void RunBatchFile()
+		{
+			System.Diagnostics.Process.Start(myBatchFileName);
+		}
+		static void AddUser()
+		{
+			Console.WriteLine("------Add User--------");
+			Console.Write("Enter New User Name:");
+			string UserName = Console.ReadLine();
+			Console.Write("Enter New Password:");
+			string Password = Console.ReadLine();
+			Console.Write("Permission Level ADMIN/NORMAL:");
+			string Permission = Console.ReadLine();
+
+
+			Security.CreateLogin(UserName, Password,Permission.ToUpper());
+			AdminPermissions();
+
+
+		}
+
+
+
+
+
+	}
+
+
+}
---
+++ Security.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+	public static class Security
+	{
+		public static string FilePath { get { return "Users.bin"; } }
+		public static void CreateLogin(string User,string Pass,string Perm)
+		{
+			List<Login> Logins;
+			Login newLogin = new Login() { UserName = User, Password = Pass, Permissions = Perm };
+			if (File.Exists(FilePath))
+			{
+				Logins = GetLogins();
+			}
+			else
+			{
+				Logins = new List<Login>();
+			}
+				
+			Logins.Add(newLogin);
+			writeLogins(Logins);
+		}
+		public static void CreateDefault()
+		{
+			CreateLogin("Clark","DaddyLovesYou","ADMIN");
+		}
+		public static List<Login> GetLogins()
+		{
+
+			if (!File.Exists(FilePath)) { CreateDefault(); }
+			List<Login> Logins;
+			Logins = new List<Login>();
+			BinaryFormatter binaryFormatter = new BinaryFormatter();
+			using (FileStream fs = new FileStream(FilePath, FileMode.Open))
+			{
+				Logins= (List<Login>)binaryFormatter.Deserialize(fs);
+			}
+			return Logins;
+		}
+		public static void writeLogins(List<Login> Logins)
+		{
+			BinaryFormatter binaryFormatter = new BinaryFormatter();
+			using (FileStream fs = new FileStream(FilePath,FileMode.Create))
+			{
+				binaryFormatter.Serialize(fs, Logins);
+			}
+		}
+
+		public static string TestCredentials(string UserName, string Password)
+		{
+			List<Login> Logins = GetLogins();
+			Logins = Logins.Where(p => p.UserName.ToUpper() == UserName.ToUpper()).ToList();
+			Logins = Logins.Where(p => p.Password == Password).ToList();
+
+			if (Logins.Count >0)
+			{ return Logins[0].Permissions; }
+			else
+			{
+				return "No Permission";
+			}
+		}
+
+	}
+
+	[Serializable]
+	public class Login
+	{
+		public string UserName { get; set; }
+		public string Password { get; set; }
+		public string Permissions { get; set; }
+
+	}
+}
